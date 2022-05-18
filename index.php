05c52563f5a11b4bd84667d668d3e2ce1cc39c5d
sycatle <sycatle@pm.me>
2022-05-18 17:15:59 +0200

    Restructuring

--- index.php
+++ index.php
@@ -1,6 +1,8 @@
 ﻿<?php
 
-/* require('./src/Router.php');
+/* -- Future optimisation du routeur. --
+
+require('./src/Router.php');
 $router = new \sycatle\beblio\Router(
     $_GET['r'] != null || $_GET['r'] != "" ? 
         $_GET['r'] 
@@ -8,39 +10,74 @@ $router = new \sycatle\beblio\Router(
         'main'
 ); */
 
-// Le fichier index.php est utilisé ici comme routeur, il appelle des contrôleurs spécifiques aux demandes de l'utilisateur.
-if (isset($_GET["r"])) {
-    if ($_GET["r"] == "connect") { // Appelle le contrôleur "Connect" afin d'afficher la page à l'utilisateur.
-        require("./controllers/c_connect.php");
-    } else if ($_GET["r"] == "explore") { // Appelle le contrôleur "Post" afin d'afficher la page à l'utilisateur.
-        require("./controllers/c_explore.php");
-    } else if ($_GET["r"] == "post") { // Appelle le contrôleur "Post" afin d'afficher la page à l'utilisateur.
-        require("./controllers/c_post.php");
-    } else if ($_GET["r"] == "library") { // Appelle le contrôleur "Post" afin d'afficher la page à l'utilisateur.
-        require("./controllers/c_library.php");
-    } else if ($_GET["r"] == "settings") { // Appelle le contrôleur "Post" afin d'afficher la page à l'utilisateur.
-        require("./controllers/c_settings.php");
-    } else if ($_GET["r"] == "admin") { // Appelle le contrôleur "Post" afin d'afficher la page à l'utilisateur.
-        require("./controllers/c_admin.php");
-    } else if ($_GET["r"] == "disconnect") { // Appelle le contrôleur "Disconnect" afin de deconnecter page à l'utilisateur.
-        require("./controllers/c_disconnect.php");    
-    } else {
-        header("Location: ./?error=404"); // Renvoie vers la page principale si la requête est invalide.
+// -- Routeur actuel, à optimiser !
+
+$getter_r = $_GET['r'];
+if (isset($getter_r)) {
+    switch ($getter_r){
+        case 'connect':
+            require("./src/ConnectController.php");
+            exit();
+        case 'explore':
+            require("./src/ExploreController.php");
+            exit();
+        case 'post':
+            require("./src/PostController.php");
+            exit();
+        case 'library':
+            require("./src/PostController.php");
+            exit();
+        case 'settings':
+            require("./src/SettingsController.php");
+            exit();
+        case 'admin':
+            require("./src/AdminController.php");
+            exit();
+        case 'disconnect':
+            require("./src/DisconnectController.php");
+            exit();
+        default:
+            header("Location: ./?error=404");
+            exit();
     }
+
+/* à OPTIMISER AUSSI.*/
+
 } else if (isset($_GET["book"])) {
-    require("./controllers/c_book.php");
+    require("./src/BookController.php");
 } else if (isset($_GET["author"])) {
-    require("./controllers/c_author.php");
+    require("./src/AuthorController.php");
 } else if (isset($_GET["quote"])) {
-    require("./controllers/c_quote.php");
+    require("./src/QuoteController.php");
 } else if (isset($_GET["category"])) {
-    require("./controllers/c_category.php");
+    require("./src/CategoryController.php");
 } else if (isset($_GET["user"])) {
-    require("./controllers/c_user.php");
+    require("./src/UserController.php");
 } else if (isset($_GET["admin"])) {
-    require("./controllers/c_admin.php");
+    require("./src/AdminController.php");
 } else if (isset($_GET["error"])) {
-    require("./controllers/c_error.php");
+    require("./src/ErrorController.php");
 } else {
-    require("./controllers/c_home.php"); // Envoie vers la page principale si aucune requête n'est spécifiée.
+    require("./src/home.php"); 
 }
+
+
+
+/* -- ANCIEN ROUTEUR TRèS MOCHE
+
+    if ($_GET["r"] == "connect") { // Appelle le contrôleur "Connect" afin d'afficher la page à l'utilisateur.
+        require("./src/ConnectController.php");
+    } else if ($_GET["r"] == "explore") { // Appelle le contrôleur "Post" afin d'afficher la page à l'utilisateur.
+        require("./src/ExploreController.php");
+    } else if ($_GET["r"] == "post") { // Appelle le contrôleur "Post" afin d'afficher la page à l'utilisateur.
+        require("./src/PostController.php");
+    } else if ($_GET["r"] == "library") { // Appelle le contrôleur "Post" afin d'afficher la page à l'utilisateur.
+        require("./src/LibraryController.php");
+    } else if ($_GET["r"] == "settings") { // Appelle le contrôleur "Post" afin d'afficher la page à l'utilisateur.
+        require("./src/SettingsController.php");
+    } else if ($_GET["r"] == "admin") { // Appelle le contrôleur "Post" afin d'afficher la page à l'utilisateur.
+        require("./src/AdminController.php");
+    } else if ($_GET["r"] == "disconnect") { // Appelle le contrôleur "Disconnect" afin de deconnecter page à l'utilisateur.
+        require("./src/DisconnectController.php");    
+    }
+*/
