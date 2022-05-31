723d2da5ad9cd30cd4dd6598cfb5f6ae17e7be36
sycatle <sycatle@pm.me>
2022-05-31 22:36:56 +0200

    Setting up themes, redesign..

--- src/templates/template.php
+++ src/templates/template.php
@@ -16,14 +16,14 @@ $user = isset($_SESSION['id']) ? new \sycatle\beblio\entity\User($_SESSION['id']
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <meta name="description" content="<?= $pageDescription ?>">
 
-  <!-- TAILWIND CSS START -->
+  <!-- TAILWIND CSS START --> <!--
   <script src="https://cdn.tailwindcss.com"></script>
-   <!-- TAILWIND CSS END -->
+  --> <!-- TAILWIND CSS END -->
 
-  <!-- BOOTSTRAP START
+  <!-- BOOTSTRAP START -->
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-   BOOTSTRAP END -->
+  <!-- BOOTSTRAP END -->
 
   <!-- FLICKITY START -->
   <link href="https://cdnjs.cloudflare.com/ajax/libs/flickity/3.0.0/flickity.min.css" rel="stylesheet" />
